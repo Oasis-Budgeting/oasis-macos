a6e4228c113ddf34d144631724fdae84c1bb4d8c
Surya Vamsi <d.suryavamsi@gmail.com>
2026-02-28 17:06:24 +0530

    Initial Commit

---
+++ Bucket BudgetTests/Bucket_BudgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_BudgetTests.swift
+//  Bucket BudgetTests
+//
+//  Created by Surya Vamsi on 28/02/26.
+//
+
+import Testing
+@testable import Bucket_Budget
+
+struct Bucket_BudgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Bucket BudgetUITests/Bucket_BudgetUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Bucket_BudgetUITestsLaunchTests.swift
+//  Bucket BudgetUITests
+//
+//  Created by Surya Vamsi on 28/02/26.
+//
+
+import XCTest
+
+final class Bucket_BudgetUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
